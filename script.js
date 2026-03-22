9081e8056e7ba7c2c38a00bcb0f2bee1ad132eb6
s1kkame111 <xmelan1114tt@gmail.com>
2026-03-22 16:13:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,65 @@
 const data = [
     {
         place:'Франція, кінець XIX ст',
-        title:'ОСНОВНІ',
-        title2:'ТЕМИ',
-        description:'У цьому проєкті розглядається розвиток Франції в період так званої «Прекрасної епохи». Це був час швидкого культурного та наукового прогресу, коли країна стала одним із центрів європейського мистецтва. Також розглядаються політичні події, зокрема Франко-прусська війна, утворення Третьої республіки та політичні кризи. Окрема увага приділяється видатним особистостям, які вплинули на розвиток Франції.',
-        image:'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Le_Palais_des_Tuileries_-_Le_Gray.jpg/1280px-Le_Palais_des_Tuileries_-_Le_Gray.jpg'
-    },
-    {
-        place:'Париж, кінець XIX ст',
-        title:'ПРЕКРАСНА',
-        title2:'ЕПОХА',
-        description:'«Belle Époque» — це період розквіту культури, мистецтва та науки у Франції. У цей час активно розвивалася мода, архітектура, живопис і література. Париж став центром європейського культурного життя. Саме тоді з’явилися нові художні напрямки, такі як імпресіонізм і постімпресіонізм. Люди вірили у прогрес і світле майбутнє.',
-        image:'https://upload.wikimedia.org/wikipedia/commons/d/d7/Scenes_around_Paris_-_Op%C3%A9ra_Garnier%2C_ca._1920%E2%80%9335.jpg'
-    },
-    {
-        place:'Париж, 1878–1900 рр',
-        title:'ВИСТАВКИ ТА',
-        title2:'НАУКА',
-        description:'У Франції проводилися Всесвітні виставки, які демонстрували технічні досягнення різних країн. Найвідомішою стала виставка 1889 року, під час якої була побудована Ейфелева вежа. У цей період активно розвивалася наука. Важливі відкриття зробили Луї Пастер у галузі медицини та Марія Склодовська-Кюрі у дослідженні радіоактивності.',
-        image:'https://cp12.nevsepic.com.ua/88/1350082433-0029899-www.nevsepic.com.ua.jpg'
-    },
-    {
-        place:'Франція, 1870–1875 рр',
-        title:'ПОЛІТИЧНІ',
-        title2:'ЗМІНИ',
-        description:'Після поразки у Франко-прусська війна у Франції відбулися значні зміни. Було повалено монархію і створено Третю республіку. У 1871 році виникла Паризька комуна — спроба створити нову форму влади. Ці події показали нестабільність політичної ситуації в країні.',
-        image:'https://st.violity.com/auction/uncos-images/55/276ab187e28bfbcddecb2b177879d1a0.jpg?t=69bfeb4c1627d'
-    },
-    {
-        place:'Франція, кінець XIX ст',
         title:'КРИЗИ ТА',
         title2:'РЕФОРМИ',
         description:'У Третій республіці виникали політичні кризи, серед яких найвідомішою була Справа Дрейфуса. Вона розділила суспільство на прихильників і противників справедливості. Водночас до влади приходили радикали, які проводили реформи, зокрема відокремлення церкви від держави та розвиток освіти.',
         image:'https://i.redd.it/s2mwciizobqg1.jpeg'
     },
     {
-        place:'Cappadocia - Turkey',
-        title:'Göreme',
-        title2:'Valley',
-        description:'Göreme Valley in Cappadocia is a historical marvel set against a unique geological backdrop, where centuries of wind and water have sculpted the landscape into whimsical formations. The valley is also famous for its open-air museums, underground cities, and the enchanting experience of hot air ballooning.',
-        image:'https://assets.codepen.io/3685267/timed-cards-6.jpg'
+        place:'Париж, кінець XIX ст',
+        title:'ПРЕКРАСНА',
+        title2:'ЕПОХА',
+        description:'«Belle Époque» — це період розквіту культури, мистецтва та науки у Франції. У цей час активно розвивалася 
+            мода, архітектура, живопис і література. Париж став центром європейського культурного життя. Саме тоді з’явилися нові художні напрямки, такі як імпресіонізм і постімпресіонізм. Люди вірили у прогрес і світле майбутнє.',
+        image:'https://upload.wikimedia.org/wikipedia/commons/d/d7/Scenes_around_Paris_-_Op%C3%A9ra_Garnier%2C_ca._1920%E2%80%9335.jpg'
+    },
+    {
+        place:'Париж, 1878–1900 рр',
+        title:'ВИСТАВКИ ТА',
+        title2:'НАУКА',
+        description:'У Франції проводилися Всесвітні виставки, які демонстрували технічні досягнення різних країн. Найвідомішою 
+            стала виставка 1889 року, під час якої була побудована Ейфелева вежа. У цей період активно розвивалася наука. Важливі відкриття зробили Луї Пастер у галузі медицини та Марія Склодовська-Кюрі у дослідженні радіоактивності.',
+        image:'https://cp12.nevsepic.com.ua/88/1350082433-0029899-www.nevsepic.com.ua.jpg'
+    },
+    {
+        place:'Франція, 1870–1875 рр',
+        title:'ПОЛІТИЧНІ',
+        title2:'ЗМІНИ',
+        description:'Після поразки у Франко-прусська війна у Франції відбулися значні зміни. Було повалено монархію і створено 
+            Третю республіку. У 1871 році виникла Паризька комуна — спроба створити нову форму влади. Ці події показали нестабільність політичної ситуації в країні.',
+        image:'https://st.violity.com/auction/uncos-images/55/276ab187e28bfbcddecb2b177879d1a0.jpg?t=69bfeb4c1627d'
+    },
+    {
+        place:'Франція, кінець XIX ст',
+        title:'ОСНОВНІ',
+        title2:'ТЕМИ',
+        description:'У цьому проєкті розглядається розвиток Франції в період так званої «Прекрасної епохи». Це був час швидкого 
+            культурного та наукового прогресу, коли країна стала одним із центрів європейського мистецтва. Також розглядаються політичні події, зокрема Франко-прусська війна, утворення Третьої республіки та політичні кризи. Окрема увага приділяється видатним особистостям, які вплинули на розвиток Франції.',
+        image:'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Le_Palais_des_Tuileries_-_Le_Gray.jpg/1280px-Le_Palais_des_Tuileries_-_Le_Gray.jpg'
+    },
+    {
+        place:'Франція, кінець XIX',
+        title:'МИСТЕЦТВО І',
+        title2:'СТИЛЬ',
+        description:'У період «Прекрасної епохи» у Франції активно розвивалися нові напрямки мистецтва, зокрема імпресіонізм і постімпресіонізм. Художники намагалися передати не точні деталі, а свої враження від світу — світло, рух, настрій. У картинах часто зображували природу, міське життя та повсякденні сцени
+Цей період також вплинув на моду та архітектуру. З’являлися нові стилі, будувалися красиві будівлі, а мистецтво ставало більш доступним для людей.',
+        image:'https://upload.wikimedia.org/wikipedia/commons/f/ff/Camille_Pissarro_-_Boulevard_Montmartre%2C_Spring_-_Google_Art_Project.jpg'
     },
 ]
 
 const _ = (id)=>document.getElementById(id)
-const cards = data.map((i, index)=>`<div class="card" id="card${index}" style="background-image:url(${i.image})"  ></div>`).join('')
+const cards = data.map((i, index)=><div class="card" id="card${index}" style="background-image:url(${i.image})"  ></div>).join('')
 
-const cardContents = data.map((i, index)=>`<div class="card-content" id="card-content-${index}">
+const cardContents = data.map((i, index)=><div class="card-content" id="card-content-${index}">
 <div class="content-start"></div>
 <div class="content-place">${i.place}</div>
 <div class="content-title-1">${i.title}</div>
 <div class="content-title-2">${i.title2}</div>
 
-</div>`).join('')
+</div>).join('')
 
-const sildeNumbers = data.map((_, index)=>`<div class="item" id="slide-item-${index}" >${index+1}</div>`).join('')
+const sildeNumbers = data.map((_, index)=><div class="item" id="slide-item-${index}" >${index+1}</div>).join('')
 _('demo').innerHTML =  cards + cardContents
 _('slide-numbers').innerHTML =  sildeNumbers
 
@@ -65,13 +70,13 @@ const range = (n) =>
 const set = gsap.set;
 
 function getCard(index) {
-  return `#card${index}`;
+  return #card${index};
 }
 function getCardContent(index) {
-  return `#card-content-${index}`;
+  return #card-content-${index};
 }
 function getSliderItem(index) {
-  return `#slide-item-${index}`;
+  return #slide-item-${index};
 }
 
 function animate(target, duration, properties) {
@@ -121,11 +126,11 @@ function init() {
   gsap.set(getCardContent(active), { x: 0, y: 0, opacity: 0 });
   gsap.set(detailsActive, { opacity: 0, zIndex: 22, x: -200 });
   gsap.set(detailsInactive, { opacity: 0, zIndex: 12 });
-  gsap.set(`${detailsInactive} .text`, { y: 100 });
-  gsap.set(`${detailsInactive} .title-1`, { y: 100 });
-  gsap.set(`${detailsInactive} .title-2`, { y: 100 });
-  gsap.set(`${detailsInactive} .desc`, { y: 50 });
-  gsap.set(`${detailsInactive} .cta`, { y: 60 });
+  gsap.set(${detailsInactive} .text, { y: 100 });
+  gsap.set(${detailsInactive} .title-1, { y: 100 });
+  gsap.set(${detailsInactive} .title-2, { y: 100 });
+  gsap.set(${detailsInactive} .desc, { y: 50 });
+  gsap.set(${detailsInactive} .cta, { y: 60 });
 
   gsap.set(".progress-sub-foreground", {
     width: 500 * (1 / order.length) * (active + 1),
@@ -191,42 +196,42 @@ function step() {
     const detailsActive = detailsEven ? "#details-even" : "#details-odd";
     const detailsInactive = detailsEven ? "#details-odd" : "#details-even";
 
-    document.querySelector(`${detailsActive} .place-box .text`).textContent =
+    document.querySelector(${detailsActive} .place-box .text).textContent =
       data[order[0]].place;
-    document.querySelector(`${detailsActive} .title-1`).textContent =
+    document.querySelector(${detailsActive} .title-1).textContent =
       data[order[0]].title;
-    document.querySelector(`${detailsActive} .title-2`).textContent =
+    document.querySelector(${detailsActive} .title-2).textContent =
       data[order[0]].title2;
-    document.querySelector(`${detailsActive} .desc`).textContent =
+    document.querySelector(${detailsActive} .desc).textContent =
       data[order[0]].description;
 
     gsap.set(detailsActive, { zIndex: 22 });
     gsap.to(detailsActive, { opacity: 1, delay: 0.4, ease });
-    gsap.to(`${detailsActive} .text`, {
+    gsap.to(${detailsActive} .text, {
       y: 0,
       delay: 0.1,
       duration: 0.7,
       ease,
     });
-    gsap.to(`${detailsActive} .title-1`, {
+    gsap.to(${detailsActive} .title-1, {
       y: 0,
       delay: 0.15,
       duration: 0.7,
       ease,
     });
-    gsap.to(`${detailsActive} .title-2`, {
+    gsap.to(${detailsActive} .title-2, {
       y: 0,
       delay: 0.15,
       duration: 0.7,
       ease,
     });
-    gsap.to(`${detailsActive} .desc`, {
+    gsap.to(${detailsActive} .desc, {
       y: 0,
       delay: 0.3,
       duration: 0.4,
       ease,
     });
-    gsap.to(`${detailsActive} .cta`, {
+    gsap.to(${detailsActive} .cta, {
       y: 0,
       delay: 0.35,
       duration: 0.4,
@@ -283,11 +288,11 @@ function step() {
         gsap.set(getSliderItem(prv), { x: rest.length * numberSize });
 
         gsap.set(detailsInactive, { opacity: 0 });
-        gsap.set(`${detailsInactive} .text`, { y: 100 });
-        gsap.set(`${detailsInactive} .title-1`, { y: 100 });
-        gsap.set(`${detailsInactive} .title-2`, { y: 100 });
-        gsap.set(`${detailsInactive} .desc`, { y: 50 });
-        gsap.set(`${detailsInactive} .cta`, { y: 60 });
+        gsap.set(${detailsInactive} .text, { y: 100 });
+        gsap.set(${detailsInactive} .title-1, { y: 100 });
+        gsap.set(${detailsInactive} .title-2, { y: 100 });
+        gsap.set(${detailsInactive} .desc, { y: 50 });
+        gsap.set(${detailsInactive} .cta, { y: 60 });
         clicks -= 1;
         if (clicks > 0) {
           step();
